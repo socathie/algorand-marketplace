a0bb099fa5c3d2bd00ddbf80bae5febde80d690e
Cathie So <socathie@gmail.com>
2022-09-18 21:56:07 +0800

    Tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Cover from "./components/Cover";
 import './App.css';
+import Wallet from "./components/Wallet";
+import { Container, Nav } from "react-bootstrap";
+import Products from "./components/marketplace/Products";
+import {Notification} from "./components/utils/Notifications";
+import { indexerClient, myAlgoConnect } from "./utils/constants";
+import coverImg from "./assets/img/sandwich.jpg"
 
-function App() {
+const App = function AppWrapper() {
+
+  const [address, setAddress] = useState(null);
+  const [name, setName] = useState(null);
+  const [balance, setBalance] = useState(0);
+
+  const fetchBalance = async (accountAddress) => {
+    indexerClient.lookupAccountByID(accountAddress).do()
+      .then(response => {
+        const _balance = response.account.amount;
+        setBalance(_balance);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const connectWallet = async () => {
+    myAlgoConnect.connect()
+      .then(accounts => {
+        const _account = accounts[0];
+        setAddress(_account.address);
+        setName(_account.name);
+        fetchBalance(_account.address);
+      }).catch(error => {
+        console.log('Could not connect to MyAlgo wallet');
+        console.error(error);
+      })
+  };
+
+  const disconnect = () => {
+    setAddress(null);
+    setName(null);
+    setBalance(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Notification />
+      {address ? (
+        <Container fluid="md">
+          <Nav className="justify-content-end pt-3 pb-5">
+            <Nav.Item>
+              <Wallet
+                address={address}
+                name={name}
+                amount={balance}
+                disconnect={disconnect}
+                symbol={"ALGO"}
+              />
+            </Nav.Item>
+          </Nav>
+          <main>
+            <Products address={address} fetchBalance={fetchBalance}/>
+          </main>
+        </Container>
+      ) : (
+        <Cover name={"Street Food"} coverImg={coverImg} connect={connectWallet} />
+      )}
+    </>
   );
 }
 
